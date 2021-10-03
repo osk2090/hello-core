890e4879ec02e9665a6794142a99f980dccbef42
osk2090 <osk2090@naver.com>
2021-10-03 18:08:51 +0900

    test

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,26 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
 
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     private MemberRepository memberRepository() {//리펙토링
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {//리펙토링
         return new FixDiscountPolicy();
     }
--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -8,28 +8,32 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class ApplicationContextInfoTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-
+    AnnotationConfigApplicationContext ac = new
+            AnnotationConfigApplicationContext(AppConfig.class);
     @Test
     @DisplayName("모든 빈 출력하기")
     void findAllBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name = " + beanDefinitionName + "object = " + bean);
+            System.out.println("name=" + beanDefinitionName + " object=" +
+                    bean);
         }
     }
 
     @Test
     @DisplayName("애플리케이션 빈 출력하기")
     void findApplicationBean() {
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();//스프링에 등록된 모든 빈 이름을 조회한다.
         for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-
+            BeanDefinition beanDefinition =
+                    ac.getBeanDefinition(beanDefinitionName);//메타 데이터를 가져옴
+            //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
-                Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = " + beanDefinitionName + "object = " + bean);
+                Object bean = ac.getBean(beanDefinitionName);//빈 이름으로 등록된 빈 객체를 조회한다.
+                System.out.println("name=" + beanDefinitionName + " object=" +
+                        bean);
             }
         }
     }
