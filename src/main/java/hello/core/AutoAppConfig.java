e567e6115761cdbdf32c9595e6c8afe943c2581d
eunsukOH <es.oh@infinigru.com>
2021-10-05 15:31:06 +0900

    @ComponentScan and defaultScan

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",//해당 패키지의 하위 클래스를 찾는다는 어노테이션
+//        basePackageClasses = AutoAppConfig.class,//해당 클래스가 속한 패키지에서 찾는다는
+//        ->결국엔 default를 추천하므로 최상단에 해당 설정 클래스를 위치하는 것이 좋다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)//해당 클래스 제외 필터링
 )
 public class AutoAppConfig {
