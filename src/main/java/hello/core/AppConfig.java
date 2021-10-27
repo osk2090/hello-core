426c48c7f5402aac61ef071afb1ddfebd5b24060
eunsukOH <es.oh@infinigru.com>
2021-10-27 17:04:26 +0900

    @Nullable and Optional<>

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -11,18 +11,13 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class AppConfig {
-
-    // @Bean memberService -> new MemoryMemberRepository()
-    // @Bean orderService -> new MemoryMemberRepository()
-    //
 
     @Bean
     public MemberService memberService() {
         System.out.println("call AppConfig.memberService");
-//        return new MemberServiceImpl(memberRepository());
-        return null;
+        return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
@@ -34,8 +29,7 @@ public class AppConfig {
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
-//        return new OrderServiceImpl(memberRepository(), discountPolicy());
-        return null;
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
